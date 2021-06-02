676fe62bd9eb15caff54d8497eede09b3a9c8714
anca6889 <guth.jeanmaxime@gmail.com>
2021-06-02 22:31:45 +0200

    Avancement FRONTEND

--- app/urls.py
+++ app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.product, name="product"),
+    path('', views.main, name="main"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from app.models import Product
+# from app.models import Product
 
-def product(request):
-    product = Product.object.all()
-    return render(request, "product.html")
-    
+def main(request):
+    return render(request, 'index.html')
+
+# def product(request):
+#     product = Product.objects.all()
+#     return render(request, "product.html")
