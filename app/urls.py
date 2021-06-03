a857870eb6222f09833d44bd81d1e200065dceea
anca6889 <guth.jeanmaxime@gmail.com>
2021-06-04 00:36:40 +0200

    Avancement FRONTEND contact et mentions légales

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from app import views
 
 urlpatterns = [
     path('', views.main, name="main"),
+    path("legal_notice/", views.get_legal_notice, name="legal_notice"),
+    path("contact/", views.get_contact, name="contact"),
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 # from app.models import Product
 
 def main(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
+
+
+def get_legal_notice(request):
+    return render(request, "legal_notice.html")
+
+
+def get_contact(request):
+    return render(request, "contact.html")
 
 # def product(request):
 #     product = Product.objects.all()
