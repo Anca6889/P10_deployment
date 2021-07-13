8eb35c6f767f2761676067aab5e818e32e4f1a58
anca6889 <guth.jeanmaxime@gmail.com>
2021-07-13 10:19:50 +0200

    test URLS OK sauf 1 (explore)

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import resolve, reverse
+from app import views
 
-# Create your tests here.
+
+class UrlsTests(SimpleTestCase):
+
+    def test_main_url_is_resolved(self):
+        url = reverse("main")
+        self.assertEquals(resolve(url).func, views.main)
+
+    def test_legals_url_is_resolved(self):
+        url = reverse("legal_notice")
+        self.assertEquals(resolve(url).func, views.get_legal_notice)
+    
+    def test_contact_url_is_resolved(self):
+        url = reverse("contact")
+        self.assertEquals(resolve(url).func, views.get_contact)
+
+    # def test_explore_url_is_resolved(self):
+    #     url = reverse("explore")
+    #     self.assertEquals(resolve(url).func, views.SearchResults)
+
+    def test_substitutes_url_is_resolved(self):
+        url = reverse("substitutes", kwargs={"product_id": 1})
+        self.assertEquals(resolve(url).func, views.get_substitutes)
+
+    def test_product_url_is_resolved(self):
+        url = reverse("product", kwargs={"product_id": 1})
+        self.assertEquals(resolve(url).func, views.get_product_details)
+
+    def test_add_favorite_url_is_resolved(self):
+        url = reverse("add_favorite", kwargs={"product_id": 1})
+        self.assertEquals(resolve(url).func, views.add_favorite)
+
+    def test_favorites_url_is_resolved(self):
+        url = reverse("favorites",)
+        self.assertEquals(resolve(url).func, views.favorites_list)
+
+    # py manage.py test
--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.urls import resolve, reverse
+from authentication import views
 
-# Create your tests here.
+
+class UrlsTests(SimpleTestCase):
+
+    def test_sign_in_url_is_resolved(self):
+        url = reverse("sign_in")
+        self.assertEquals(resolve(url).func, views.sign_in)
+
+    def test_login_url_is_resolved(self):
+        url = reverse("login")
+        self.assertEquals(resolve(url).func, views.sign_up)
+
+    def test_account_url_is_resolved(self):
+        url = reverse("account")
+        self.assertEquals(resolve(url).func, views.account)
+
+    def test_sign_out_url_is_resolved(self):
+        url = reverse("logout")
+        self.assertEquals(resolve(url).func, views.sign_out)
