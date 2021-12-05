f0e19b0114fcaa968b2ddc34f3c25a7408fed3cb
Jean-Maxime G <71003854+Anca6889@users.noreply.github.com>
2021-12-05 10:57:28 +0100

    Separation environnement PRDO / DEV

--- P8_pur_beurre/settings/__init__.py
+++ P8_pur_beurre/settings/__init__.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get("ENV", "development") == "production" else True
 
-ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapps.com', 'localhost', '127.0.0.1', '159.223.17.230']
 
 
 # Application definition
