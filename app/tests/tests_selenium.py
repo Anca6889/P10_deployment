82f65f117501d668f7d5c34f8f6c1a066f6cf57a
Jean-Maxime G <71003854+Anca6889@users.noreply.github.com>
2021-12-07 23:46:19 +0100

    essai travis 466

--- app/tests/tests_selenium.py
+++ app/tests/tests_selenium.py
@@ -4,6 +4,11 @@ from selenium import webdriver
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--remote-debugging-port=9222')
+chrome_options.add_argument('--window-size=1920x1080')
 
 class BrowserTests(StaticLiveServerTestCase):
     """This class will test a product research and click on legals link"""
@@ -11,7 +16,10 @@ class BrowserTests(StaticLiveServerTestCase):
     def setUp(self):
         """setup the webdriver with Google Chrome driver"""
 
-        self.driver = webdriver.Chrome(executable_path=(str(BASE_DIR / 'webdrivers' / 'chromedriver')))
+        self.driver = webdriver.Chrome(
+            executable_path=str(BASE_DIR / 'webdrivers' / 'chromedriver'),
+            options=chrome_options,
+        )
 
     def test_search_product_and_legals(self):
         """This method will do all the actions, check comments below"""
--- authentication/tests/tests_selenium.py
+++ authentication/tests/tests_selenium.py
@@ -6,6 +6,11 @@ import random
 import string
 import time
 
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--remote-debugging-port=9222')
+chrome_options.add_argument('--window-size=1920x1080')
 
 class BrowserTests(StaticLiveServerTestCase):
     """This class will test a registering, logout and login an user"""
@@ -13,7 +18,10 @@ class BrowserTests(StaticLiveServerTestCase):
     def setUp(self):
         """setup the webdriver with Google Chrome driver"""
 
-        self.driver = webdriver.Chrome(executable_path=(str(BASE_DIR / 'webdrivers' / 'chromedriver')))
+        self.driver = webdriver.Chrome(
+            executable_path=str(BASE_DIR / 'webdrivers' / 'chromedriver'),
+            options=chrome_options,
+        )
 
     def test_login_logout_signin(self):
         """This method will do all the actions, check comments below"""
